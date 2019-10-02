26973db655193918b4cdd662fa236e50084c8201
Sanjar <fatality1@mail.ru>
2019-10-02 07:51:00 +0500

    init: start react ajax project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Blog from './containers/Blog/Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
+  }
+}
+
+export default App
